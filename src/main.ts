482db26e39930305bdf24154ea927352f9dab6ed
jyx-07 <s25069@gsm.hs.kr>
2026-09-23 08:42:21 +0900

    fix(global): 종료 알림 안 옴 - enableShutdownHooks 누락 수정

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
   app.useGlobalPipes(new ZodValidationPipe());
 
   const config = app.get(ConfigService);
