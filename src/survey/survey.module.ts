f2d0ed67cce401a87786c6f339ebfe555c7e98f3
jyx-07 <s25069@gsm.hs.kr>
2026-09-22 18:20:45 +0900

    feat(survey): SurveyEntity·DynamicSurveyEntity 추가

--- src/survey/survey.module.ts
+++ src/survey/survey.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DynamicSurveyEntity } from './entities/dynamic-survey.entity.js';
+import { SurveyEntity } from './entities/survey.entity.js';
 import { SurveyController } from './survey.controller.js';
 import { SurveyService } from './survey.service.js';
 
+/** survey 도메인(설문 정의 + 문항) 모듈. */
 @Module({
+  imports: [TypeOrmModule.forFeature([SurveyEntity, DynamicSurveyEntity])],
   controllers: [SurveyController],
   providers: [SurveyService],
 })
